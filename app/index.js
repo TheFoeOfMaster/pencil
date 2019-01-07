bb985c2cf9f67585fee936e49773ad6e53c1db9a
TheFoeOfMaster <931976350@qq.com>
2019-01-07 18:49:07 +0800

    Update index.js

--- app/index.js
+++ app/index.js
@@ -1,22 +1,34 @@
+//严格模式，不能使用未声明的变量
 "use strict";
 
+//const声明常量，被声明的常量不能更改且必须初始化。require似乎是node的模块。
 const {app, protocol, shell, BrowserWindow} = require("electron");
+
+//此变量似乎是用来描述npm包信息
 const pkg      = require("./package.json");
+
+//此模块是node中的文件系统模块
 const fs       = require("fs");
+
+//此模块是node中的路径模块,提供了一些处理文件路径的工具
 const path     = require("path");
 
+//下面两行代码作用是 为electron添加命令行开关以便其能访问本地文件
 app.commandLine.appendSwitch("allow-file-access-from-files");
 app.commandLine.appendSwitch("allow-file-access");
 
 // Disable hardware acceleration by default for Linux
 // TODO: implement a setting for this one and requires a restart after changing that value
+//默认情况下禁用linux的硬件加速
 if (process.platform.trim().toLowerCase() == "linux" && app.disableHardwareAcceleration) {
     console.log("Hardware acceleration disabled for Linux.");
     app.disableHardwareAcceleration();
 }
 
+//为node中的global对象增加一个shareObject属性，其值是app参数，process.的命令行参数
 global.sharedObject = { appArguments: process.argv };
 
+//处理重定向
 var handleRedirect = (e, url) => {
     e.preventDefault();
     shell.openExternal(url);
